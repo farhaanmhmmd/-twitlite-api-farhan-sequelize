6848c6db168ea73c3c605763ab21994157f71520
farhaanmhmmd <farhanmhmmd96@gmail.com>
2022-07-17 09:35:55 +0700

    Initializing

--- index.js
+++ index.js
@@ -3,19 +3,13 @@ const app = express();
 const port = 2104;
 const bearerToken = require("express-bearer-token");
 
-// ROUTERS
-const userRouter = require("./routers/user");
-
 app.use(bearerToken());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API JALAN MZ 🚀");
+  res.send("API is running");
 });
 
-app.use("/users", userRouter);
-
-// error handler
 app.use((error, req, res, next) => {
   console.log({error});
 
@@ -27,9 +21,4 @@ app.use((error, req, res, next) => {
 
   const httpCode = error.code ? error.code : 500;
   res.status(httpCode).send(errorObj);
-});
-
-app.listen(port, (error) => {
-  if (error) return console.log({err: error.message});
-  console.log(`API berhasil running di port ${port}`);
 });
--- src/lib/token/index.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-const SECRET_WORD = "strong_mountain";
-
-const createToken = (payload) => jwt.sign(payload, SECRET_WORD);
-const verifyToken = (token) => jwt.verify(token, SECRET_WORD);
-
-module.exports = {createToken, verifyToken};
