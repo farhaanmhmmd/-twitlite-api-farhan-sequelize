75dbc3fcd8c0313d0dca58d5e26fec3cd910c1e3
farhaanmhmmd <farhanmhmmd96@gmail.com>
2022-07-20 18:57:52 +0700

    Setup Connection and Starting Migration from MySQL2 to Sequelize

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
-const port = 2105;
+const port = 2104;
 const bearerToken = require("express-bearer-token");
-require("./config/database");
+const cors = require("cors");
 
+const userRouter = require("./routers/user");
+// const postRouter = require("./router/home")
+
+app.use(cors());
 app.use(bearerToken());
+app.use("/public", express.static("public"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running");
+  res.send("API JALAN MZ 🚀");
 });
+
+app.use("/users", userRouter);
+// app.use("/posts", postRouter);
 
 app.use((error, req, res, next) => {
   console.log({error});
@@ -20,11 +28,11 @@ app.use((error, req, res, next) => {
     detail: error,
   };
 
-  const httpCode = error.code ? error.code : 500;
+  const httpCode = typeof error.code == "number" ? error.code : 500;
   res.status(httpCode).send(errorObj);
 });
 
 app.listen(port, (error) => {
   if (error) return console.log({err: error.message});
-  console.log(`API successfully running in ${port}`);
+  console.log(`API is running in port ${port}`);
 });
